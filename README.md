3eb7ef096746bdfe50391598b9e0957338bf53bf
zhuzidian <zhuzidian@gmail.com>
2022-08-12 11:03:08 +0900

    use flag

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ur-room-searcher
 
-```
-go run . "70000" "1R,1K|1DK|1LDK"
+```bash
+go run main.go -rent-high="80000" -room-type="1R,1K|1DK|1LDK"
+./ur-room-searcher -rent-high="80000" -room-type="1R,1K|1DK|1LDK"
 ```
--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -17,8 +18,8 @@ type Bukken struct {
 	Rent      string `json:"rent"`
 	Commonfee string `json:"commonfee"`
 	RoomCount int    `json:"roomCount"`
-	Skcs      string `json:"skcs"`
-	BukkenUrl string `json:"bukkenUrl"` // /chintai/kanto/kanagawa/40_0660.html
+	Skcs      string `json:"skcs"`      // 市区町村
+	BukkenUrl string `json:"bukkenUrl"` // 例：/chintai/kanto/kanagawa/40_0660.html
 }
 
 type Room struct {
@@ -30,14 +31,18 @@ type Room struct {
 	Type      string `json:"type"`
 }
 
-var rent = "80000"
-var roomType = "1R,1K|1DK|1LDK"
+var rentLow int
+var rentHigh int
+var roomType string
 
 func main() {
-	rent = os.Args[1]
-	roomType = os.Args[2]
+	flag.IntVar(&rentLow, "rent-log", 30000, "家賃FROM。30000")
+	flag.IntVar(&rentHigh, "rent-high", 80000, "家賃TO。80000")
+	flag.StringVar(&roomType, "room-type", "1R,1K|1DK|1LDK", "間取り。1R,1K|1DK|1LDK")
+	flag.Parse()
 
 	fmt.Print("\n☆☆☆ 神奈川 ☆☆☆\n\n")
+	time.Sleep(5 * time.Second)
 	search("14", "01")
 	search("14", "02")
 	search("14", "03")
@@ -46,6 +51,7 @@ func main() {
 	search("14", "06")
 
 	fmt.Print("\n☆☆☆ 東京 ☆☆☆\n\n")
+	time.Sleep(5 * time.Second)
 	search("13", "01")
 	search("13", "02")
 	search("13", "03")
@@ -53,56 +59,52 @@ func main() {
 	search("13", "05")
 	search("13", "06")
 
-	time.Sleep(5 * time.Second)
-
 	fmt.Print("\n☆☆☆ 埼玉 ☆☆☆\n\n")
+	time.Sleep(5 * time.Second)
 	search("11", "01")
 	search("11", "02")
 	search("11", "03")
 	search("11", "04")
 
-	time.Sleep(5 * time.Second)
-
 	fmt.Print("\n☆☆☆ 千葉 ☆☆☆\n\n")
+	time.Sleep(5 * time.Second)
 	search("12", "01")
 	search("12", "02")
 	search("12", "03")
 	search("12", "04")
-	// search("12", "05")
-	// search("12", "06")
-	// search("12", "07")
-
-	time.Sleep(5 * time.Second)
+	search("12", "05")
+	search("12", "06")
+	search("12", "07")
 
 	fmt.Print("\n☆☆☆ 京都 ☆☆☆\n\n")
+	time.Sleep(5 * time.Second)
 	search("26", "01")
 
-	time.Sleep(5 * time.Second)
-
 	fmt.Print("\n☆☆☆ 大阪 ☆☆☆\n\n")
+	time.Sleep(5 * time.Second)
 	search("27", "01")
-	// search("27", "02")
+	search("27", "02")
 }
 
 // tdkf: 都道府県
 func search(tdfk string, area string) {
-	bukkens := fetchBukken(tdfk, area)
+	bukkens := getBukken(tdfk, area)
 	for _, bukken := range bukkens {
 		if bukken.RoomCount == 0 {
 			continue
 		}
 		fmt.Printf("%s | %s | %s \n", bukken.Name, bukken.Skcs, "https://www.ur-net.go.jp"+bukken.BukkenUrl)
-		rooms := fetchRoom(tdfk, bukken)
+		rooms := getRoom(tdfk, bukken)
 		for _, room := range rooms {
 			fmt.Printf("\t%s | %s | %s \n", room.Name, room.Rent+room.Commonfee, room.Type)
 		}
 	}
 }
 
-func fetchBukken(tdfk string, area string) []Bukken {
+func getBukken(tdfk string, area string) []Bukken {
 	formData := url.Values{
-		"rent_high": []string{rent},
-		"rent_low":  []string{"40000"},
+		"rent_high": []string{strconv.Itoa(rentHigh)},
+		"rent_low":  []string{strconv.Itoa(rentLow)},
 		"room":      strings.Split(roomType, "|"),
 		"tdfk":      []string{tdfk},
 		"area":      []string{area},
@@ -120,10 +122,10 @@ func fetchBukken(tdfk string, area string) []Bukken {
 	return bukkens
 }
 
-func fetchRoom(tdfk string, bukken Bukken) []Room {
+func getRoom(tdfk string, bukken Bukken) []Room {
 	formData := url.Values{
-		"rent_high": []string{rent},
-		"rent_low":  []string{"40000"},
+		"rent_high": []string{strconv.Itoa(rentHigh)},
+		"rent_low":  []string{strconv.Itoa(rentLow)},
 		"room":      strings.Split(roomType, "|"),
 		"tdfk":      []string{tdfk},
 		"mode":      []string{"init"},
